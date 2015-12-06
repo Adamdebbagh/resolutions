efc1345d322aceaebfff9fe2686d0424cc301b7d
Adam Debbagh <adamdebbagh@gmail.com>
2015-12-06 07:57:21 -0500

    saved data to mongo with form input

---
+++ resolutions.js
@@ -0,0 +1,31 @@
+Resolutions = new Mongo.Collection('resolutions');
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    resolutions: function() {
+        return Resolutions.find();
+    }
+  });
+
+  Template.body.events({
+      // on submit, grab the title value
+     'submit .new-resolution': function(event) {
+         var title = event.target.title.value;
+     // insert into db collection
+     Resolutions.insert({
+         title: title,
+         createdAt: new Date()
+     });
+         // eliminate value from previous field
+         event.target.title.value = "";
+         // return false so the page doesn't refresh
+         return false;
+     }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
