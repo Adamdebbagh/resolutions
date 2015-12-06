3f5cb093cb534991d8c81001272dea1688d5f358
Adam Debbagh <adamdebbagh@gmail.com>
2015-12-06 08:27:26 -0500

    added delete and checked features

--- resolutions.js
+++ resolutions.js
@@ -22,6 +22,14 @@ if (Meteor.isClient) {
          return false;
      }
   });
+  Template.resolution.events({
+      'click .toggle-checked': function(){
+          Resolutions.update(this._id, {$set:{checked:!this.checked}});
+      },
+      'click .delete': function() {
+          Resolutions.remove(this._id);
+      }
+  });
 }
 
 if (Meteor.isServer) {
